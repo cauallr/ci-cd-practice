e0d23b7443ddc6008a03896f148f6fbe396b1401
cauallr <132109162+cauallr@users.noreply.github.com>
2024-11-26 20:50:21 -0300

    Update app.test.js

--- teste/app.test.js
+++ teste/app.test.js
@@ -1,5 +1,4 @@
-// app.test.js
-it('soma de 2 e 3 deve ser 5', () => {
-  const soma = (a, b) => a + b;
-  expect(soma(2, 3)).toBe(5);
+const soma = require('../app');
+test('deve somar 2 e 3 para obter 5', () => {
+ expect(soma(2, 3)).toBe(5);
 });
